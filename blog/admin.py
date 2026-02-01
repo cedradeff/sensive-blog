07d0f7dca47c18c3756f00a078f7e9f707c47d34
etcetera <cedradeff@gmail.com>
2026-02-01 19:27:15 +0500

    fix admin page

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Post)
-admin.site.register(Tag)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_at', 'likes_count', 'comments_count')
+    raw_id_fields = ('author',)
+    def likes_count(self, obj):
+        return obj.likes.count()
+    likes_count.short_description = 'Likes'
+
+    def comments_count(self, obj):
+        return obj.comments.count()
+    comments_count.short_description = 'Comments'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title', 'posts_count')
+    def posts_count(self, obj):
+        return obj.posts.count()
+    posts_count.short_description = 'Posts'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'post', 'author', 'published_at')
+    raw_id_fields = ('post', 'author')
